86e5190e98d978249d7769ba3482774d8bf11579
madmti <supermatrix9@gmail.com>
2024-08-03 20:54:50 -0400

    Simple express API

--- Portal-inf-usm-backend/src/server.ts
+++ Portal-inf-usm-backend/src/server.ts
@@ -1,1 +1,23 @@
-console.log("Hello from server");
+import express from 'express';
+import cors from 'cors';
+import { enable } from 'colors';
+import { log } from '@/lib/funcs';
+import Madlogger from '@/lib/logger';
+import authRouter from '@/routes/auth';
+import { connectDB } from './database';
+enable();
+connectDB();
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.json());
+app.use(Madlogger('dev'));
+app.use(cors());
+
+app.use('/auth', authRouter);
+
+app.listen(app.get('port'), () => {
+	log(['PORTAL:'.bgBlue, 'REST API'.cyan], 0);
+	console.log(`Running on port ${app.get('port')}\n`.cyan);
+});
